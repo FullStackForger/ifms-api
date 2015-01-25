66b208eb301cab7876737049e625864ed3f5815b
IndieForger <indieforger@gmail.com>
2015-01-25 01:09:34 +0000

    sample endpoints added

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,7 @@ routes = [{
 		method: 'GET',
 		config: handlerConfig
 	},{
-		path: '/auth',
+		path: '/auth',  //todo: authenticates and/or registers (internal redirection) and sends back game token
 		method: 'GET',
 		config: {
 			auth: 'facebook',
@@ -22,20 +22,32 @@ routes = [{
 			handler: userController.authorisationHandler
 		}
 	},{
-		path: '/user',
+		path: '/user',  //todo: sends back user data retrieved from database
 		method: 'GET',
 		config: {
 			description: 'Provides registered user data',
 			handler: userController.infoHandler
 		}
 	},{ 
-		path: '/user/register',
+		path: '/user/register', //todo: registers user with provided data (facebook, anonymous, login+pass)
 		method: 'GET', 
 		config: handlerConfig 
-	},{ 
-		path: '/score', 
-		method: 'GET, POST', 
-		config: handlerConfig 
+	},{
+		path: '/game/{id}', //todo: retrieves game info (namve,description, version) for registered with latest news
+		method: 'GET',
+		config: handlerConfig
+	},{
+		path: '/game/{id}/meta', //todo: retrieves game metadata (score keys & achievenents) for registered
+		method: 'GET',
+		config: handlerConfig
+	},{
+		path: '/game/{id}/score/{key}', //todo: saves and loads game score by defined id
+		method: 'GET, POST',
+		config: handlerConfig
+	},{
+		path: '/game/{id}/all}', //todo: retrieves all or saves bulk scores, payload: array of pairs: key, score
+		method: 'GET, POST',
+		config: handlerConfig
 }];
 
 module.exports = routes;
