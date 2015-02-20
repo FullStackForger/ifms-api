a92776fa2f123be755426e5ccced59bca3f4a503
Mark Bevels <mark.bevels@gmail.com>
2015-02-20 18:08:17 +0000

    ident generation update

--- app/helpers/ident.js
+++ app/helpers/ident.js
@@ -7,11 +7,10 @@ module.exports = {
 	 * @returns {String}
 	 */
 	toBase64: function (ident) {
-		var chain = ident.udid + ':' + ident.pkey;
+		var chain = 'Ident ' + ident.udid + ':' + ident.pkey;
 		return (new Buffer(chain, 'utf8')).toString('base64');
-
 	},
-	
+
 	/**
 	 * @param string base64 string made of pkey:udid
 	 * @returns {{pkey: *, udid: *}}
@@ -22,7 +21,17 @@ module.exports = {
 		if (!string) return null;
 
 		string = Buffer(string, 'base64').toString();
-		chunks = string.split(':');
+		chunks = string.split(' ');
+		
+		if (chunks.length != 2 || chunks[0].toLowerCase() !== 'ident') {
+			throw new Error('Signature format is invalid');
+		}
+		
+		chunks = chunks.split(':');
+		if (chunks.length != 2) {
+			throw new Error('Signature format is invalid');
+		}
+		
 		return {
 			udid: chunks[0],
 			pkey: chunks[1]
