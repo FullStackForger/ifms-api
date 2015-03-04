5191b0dff533bbb0d031aeecdc69ae75ac7d30a6
Mark Bevels <mark.bevels@gmail.com>
2015-03-04 22:21:08 +0000

    post-deploy hook updated

--- dev/bin/post-deploy.sh
+++ dev/bin/post-deploy.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-echo "no post deply scripts yet"
+expLoc='dev/bin/post-deploy.sh'
+curLoc="${BASH_SOURCE[0]}"
+
+if [ "$expLoc" != "$curLoc" ]; then
+    echo 'You can run this script from project root directory only'
+    exit 1
+fi
+
+echo "npm update"
+npm update
+
+echo "updating config.json"
+mkdir -p config
+cp dev/config/config-prod.json config/config.json
+
+echo "updating nginx config files"
+sudo cp -r dev/nginx/* /etc/nginx/
+
+echo "forcing site enabling symplink"
+sudo ln -sf /etc/nginx/sites-available/api.innocentio.com /etc/nginx/sites-enabled/api.innocentio.com
+
+echo "reloading nginx"
+sudo service nginx restart
+
+
+echo "THAT'S ALL FOLKS!"
