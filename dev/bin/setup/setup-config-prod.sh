0ef8c1936fc9e4013222af45a84fcf1ac1180e36
Mark Bevels <mark.bevels@gmail.com>
2015-03-05 18:50:59 +0000

    more changes

--- dev/bin/setup/setup-config-prod.sh
+++ dev/bin/setup/setup-config-prod.sh
@@ -13,14 +13,22 @@ export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 echo "npm update"
-npm update
+rm -Rf node_modules
+npm install
 
 echo "updating config.json"
 mkdir -p config
 cp dev/config/config-prod.json config/config.json
 
+
+if [ -e /etc/nginx/sites-enabled/default ]; then
+	sudo echo "deleting: /etc/nginx/sites-enabled/default"
+	rm /etc/nginx/sites-enabled/default
+fi
+
 echo "updating nginx config files"
 sudo cp -r dev/nginx/* /etc/nginx/
+
 
 echo "forcing site enabling symplink"
 sudo ln -sf /etc/nginx/sites-available/api.innocentio.com /etc/nginx/sites-enabled/api.innocentio.com
