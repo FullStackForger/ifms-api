c55aba112ed907648aa6d5c0b4e93d944b7e83c0
Mark Bevels <mark.bevels@gmail.com>
2015-03-05 18:33:05 +0000

    loading nvm in setup-config-prod script

--- dev/bin/setup/setup-config-prod.sh
+++ dev/bin/setup/setup-config-prod.sh
@@ -8,8 +8,9 @@ if [[ ! "$curLoc" =~ "$expLoc" ]]; then
     exit 1
 fi
 
-echo "sourcing ~/.bashrc"
-source ~/.bashrc
+echo "loading nvm"
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 echo "npm update"
 npm update
